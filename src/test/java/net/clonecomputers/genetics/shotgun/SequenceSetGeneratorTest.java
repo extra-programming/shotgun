cc62884de8827e558a73ef5abcda962993909b5e
1000000000 <lthyde@gmail.com>
2015-02-04 16:22:10 -0500

    Created test class and moved SequenceSetGenerator.java

---
+++ src/test/java/net/clonecomputers/genetics/shotgun/SequenceSetGeneratorTest.java
@@ -0,0 +1,14 @@
+package net.clonecomputers.genetics.shotgun;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SequenceSetGeneratorTest {
+
+	@Test
+	public void test() {
+		// TODO write test
+	}
+
+}
